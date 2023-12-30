4f79b76112f0105965d6b4ab7a7d04df74d7a26f
Nazimul Hoque <konoha117@hotmail.com>
2023-12-30 02:41:39 -0500

    fleshed out tests

--- Scenes/Tiles/TileSelectorGM.gd
+++ Scenes/Tiles/TileSelectorGM.gd
@@ -7,27 +7,27 @@ var floor = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	mouse_pos_on_map()
 
 func _physics_process(delta):
-	mouse_pos_on_map()
+	pass
+
 	
 
 
 func mouse_pos_on_map():
 	var mouse_position = get_viewport().get_mouse_position()
-	#print(mouse_position)
 	
+	print(mouse_position)
+	move_ui_to_place(mouse_position)
 	ray_origin = $"../Camera3D".project_ray_origin(mouse_position)
 	ray_target = ray_origin + $"../Camera3D".project_ray_normal(mouse_position) * 2000
 	#print(ray_origin, ray_target)
 	var space_state = get_world_3d().direct_space_state
-	
 	#PhysicsRayQueryParameters3D is required to create parameters for space state in godot 4
 	var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_target)
 	var intersection = space_state.intersect_ray(query)
@@ -35,16 +35,18 @@ func mouse_pos_on_map():
 	#the meshlibrary later
 	if not intersection.is_empty():
 		var pos = intersection.position
-		#print(pos)
+		print(pos)
 		var look_at_me = Vector3(pos.x, floor, pos.z)
 		var loc_vec = to_local(look_at_me)
 		#print(loc_vec)
 		var local_pos = local_to_map(loc_vec)
 		#print(local_pos)
-		#set_cell_item(local_pos, 36, 0)
+		set_cell_item(local_pos, 36, 0)
 		#print(look_at_me)
 		
-		
 
+
+func move_ui_to_place(mousepos):
+	get_node("../towerUI/UI").set_position(mousepos)
 
 
---
+++ ViewportCapture.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+var img
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	await RenderingServer.frame_post_draw
+	$SubViewportContainer/SubViewport.get_texture().get_image().save_png("tower_thumb.png")
+	
+func _process(delta):
+	pass
+
+	
+	
+	
+	
+	
+	
+
+
